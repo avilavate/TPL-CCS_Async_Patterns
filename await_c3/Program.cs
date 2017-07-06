edbb91dc3f4aa38cc7b3c1bffd7f1aa01ac29a05
avilavate <avilavate123@gmail.com>
2017-07-06 11:15:55 +0530

    Added await_c3 project compares Task based continueWith and await approach.

---
+++ await_c3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace await_c3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessAsync();
+            ProcessTask();
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(50);
+                Console.WriteLine(i);
+            }
+
+        }
+
+        private static async Task ProcessAsync()
+        {
+            string res;
+            using (var wc= new WebClient())
+            {
+                res = await wc.DownloadStringTaskAsync(@"https://jsonplaceholder.typicode.com/posts/1");
+            }
+
+            Console.WriteLine("Async: "+res);
+        }
+        private static  void ProcessTask()
+        {
+            try
+            {
+                using (var wc = new WebClient())
+                {
+                    wc.DownloadStringTaskAsync(@"https://jsonplaceholder.typicode.com/posts/1").ContinueWith(t => {
+                        Task.Delay(90);
+                        Console.WriteLine("TPL Task: "+t.Result);
+                    });
+                }
+            }
+            catch (Exception x)
+            {
+            }
+            
+        }
+    }
+}
