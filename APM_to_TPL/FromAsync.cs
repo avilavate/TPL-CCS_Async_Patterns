d359e3f24825c6b81aa1503bda465729818334af
avilavate <avilavate123@gmail.com>
2017-07-07 16:53:24 +0530

    Minor changes.

--- APM_to_TPL/FromAsync.cs
+++ APM_to_TPL/FromAsync.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +12,33 @@ namespace APM_to_TPL
     {
         public static Task<int> ReadAsynTask(this Stream s, byte[] buffer)
         {
-          
             return Task<int>.Factory.FromAsync(s.BeginRead, s.EndRead, buffer, 0, 10000, null);
+        }
+
+        public static  void Get()
+        {
+            HttpClient ht = new HttpClient();
+            try
+            {
+                ht.GetStringAsync(@"https://github.com/avilavate").ContinueWith(
+                     t =>
+                            {
+                                Console.WriteLine(t.Result.Substring(0,2600));
+                            });
+
+                Task.Factory.StartNew(
+                     ()=> 
+                            {
+                                for (int i = 0; i < 100; i++)
+                                {
+                                    Console.WriteLine(i);
+                                }
+                            });
+            }
+            catch(Exception e)
+            {
+
+            }
         }
     }
 }
--- APM_to_TPL/Program.cs
+++ APM_to_TPL/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,17 +12,17 @@ namespace APM_to_TPL
             Task<int> t_read;var buffer = new byte[10000] ;
             FileStream fileStream = new FileStream(@"ratings.txt", FileMode.Open, FileAccess.Read);
             t_read = fileStream.ReadAsynTask(buffer);
-
+            
             var t_1 = t_read.ContinueWith(t =>
               {
                   Console.WriteLine(t.Result);
                   Console.WriteLine(buffer.Length);
-                  Console.WriteLine(System.Text.Encoding.UTF8.GetString(buffer));
+                  Console.WriteLine(Encoding.UTF8.GetString(buffer));
                   fileStream.Close();
               });
-
-            t_1.Wait();
-
+            
+            FromAsync.Get();
+        
             Console.Read();
         }
     }
