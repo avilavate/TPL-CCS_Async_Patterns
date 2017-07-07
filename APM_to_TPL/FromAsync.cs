0b04da4f118835dcbb074d31e244adddc6e59313
avilavate <avilavate123@gmail.com>
2017-07-07 15:07:44 +0530

    iA sample file read using TPl's FromAsyn.

---
+++ APM_to_TPL/FromAsync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APM_to_TPL
+{
+    static class FromAsync
+    {
+        public static Task<int> ReadAsynTask(this Stream s, byte[] buffer)
+        {
+            return Task<int>.Factory.FromAsync(s.BeginRead, s.EndRead, buffer, 0, 100, null);
+        }
+    }
+}
---
+++ APM_to_TPL/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APM_to_TPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> t_read;var buffer = new byte[100] ;
+            FileStream fileStream = new FileStream(@"ratings.txt", FileMode.Open, FileAccess.Read);
+            t_read = fileStream.ReadAsynTask(buffer);
+
+            var t_1 = t_read.ContinueWith(t =>
+              {
+                  Console.WriteLine(t.Result);
+                  Console.WriteLine(buffer.Length);
+                  fileStream.Close();
+              });
+
+            t_1.Wait();
+
+            Console.Read();
+        }
+    }
+}
