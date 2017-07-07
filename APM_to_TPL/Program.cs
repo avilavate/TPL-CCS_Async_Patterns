c9eca3ab4d74ffbefd2dc009112c1e4e6bf88755
avilavate <avilavate123@gmail.com>
2017-07-07 16:58:06 +0530

    Code cleanup.

--- APM_to_TPL/Program.cs
+++ APM_to_TPL/Program.cs
@@ -9,20 +9,20 @@ namespace APM_to_TPL
     {
         static void Main(string[] args)
         {
-            Task<int> t_read;var buffer = new byte[10000] ;
+            var buffer = new byte[10000];
             FileStream fileStream = new FileStream(@"ratings.txt", FileMode.Open, FileAccess.Read);
-            t_read = fileStream.ReadAsynTask(buffer);
-            
-            var t_1 = t_read.ContinueWith(t =>
-              {
-                  Console.WriteLine(t.Result);
-                  Console.WriteLine(buffer.Length);
-                  Console.WriteLine(Encoding.UTF8.GetString(buffer));
-                  fileStream.Close();
-              });
-            
+
+            fileStream.ReadAsynTask(buffer)
+                .ContinueWith(
+                        t =>
+                              {
+                                  Console.WriteLine(t.Result);
+                                  Console.WriteLine(buffer.Length);
+                                  Console.WriteLine(Encoding.UTF8.GetString(buffer));
+                                  fileStream.Close();
+                              });
+
             FromAsync.Get();
-        
             Console.Read();
         }
     }
